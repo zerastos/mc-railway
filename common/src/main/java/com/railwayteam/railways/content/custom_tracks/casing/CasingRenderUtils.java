fa36df2146aec050f96f22543c6ac536dd697d15
Zerastos <30709206+zerastos@users.noreply.github.com>
2026-09-08 22:06:57 +0200

    OPtimize CasingRenderUtils duplicate detection
    Change from O(n²) to O(1) and avoid allocating some arrays/a final copied list.

--- common/src/main/java/com/railwayteam/railways/content/custom_tracks/casing/CasingRenderUtils.java
+++ common/src/main/java/com/railwayteam/railways/content/custom_tracks/casing/CasingRenderUtils.java
@@ -38,6 +38,8 @@ import dev.engine_room.flywheel.lib.model.SimpleModel;
 import dev.engine_room.flywheel.lib.model.baked.BakedModelBuilder;
 import dev.engine_room.flywheel.lib.model.baked.PartialModel;
 import dev.engine_room.flywheel.lib.transform.TransformStack;
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
+import it.unimi.dsi.fastutil.longs.LongSet;
 import net.createmod.catnip.data.Iterate;
 import net.createmod.catnip.data.Pair;
 import net.createmod.catnip.render.CachedBuffers;
@@ -46,6 +48,7 @@ import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -159,7 +162,7 @@ public abstract class CasingRenderUtils {
 
     public static List<Vec3> casingPositions(BezierConnection bc) {
         List<Vec3> positions = new ArrayList<>();
-        List<int[]> takenPositions = new ArrayList<>();
+        LongSet takenPositions = new LongOpenHashSet();
         for (BezierConnection.Segment segment : bc) {
             double factor = 1.3;
             if (bc.getMaterial().trackType == WIDE_GAUGE)
@@ -177,14 +180,14 @@ public abstract class CasingRenderUtils {
             for (int i = 0; i <= steps; i++) {
                 int x = (int) Math.floor(curPos.x);
                 int z = (int) Math.floor(curPos.z);
-                if (takenPositions.stream().noneMatch(pos -> pos[0] == x && pos[1] == z)) {
-                    takenPositions.add(new int[]{x, z});
+
+                if (takenPositions.add(ChunkPos.asLong(x, z))) {
                     positions.add(new Vec3(x, curPos.y - (3 / 16d), z));
                 }
                 curPos = curPos.add(stepVec);
             }
         }
-        return positions.stream().toList();
+        return positions;
     }
 
     public static TransformedInstance makeCasingInstance(PartialModel baseModel, Block casingBlock, InstancerProvider instancerProvider) {
